f9577035f312615eb620039e74383532fbd0094a
Charles Boissy <Charles Boissy>
2017-12-05 12:34:44 +0100

    Changements inscription et tout

--- app/config/router.php
+++ app/config/router.php
@@ -8,6 +8,7 @@ $routes = [
     ['/\/member\/(?<id>[0-9]+)/', ['\Src\Controllers\IndexController', 'member']],
     ['/\/login/', ['\Src\Controllers\UserController', 'login']],
     ['/\/register/', ['\Src\Controllers\UserController', 'register']],
+    ['/\/registerpost/', ['\Src\Controllers\UserController', 'registerpost']],
     ['/\/loginpost/', ['\Src\Controllers\UserController', 'loginpost']],
     ['/\/homes/', ['\Src\Controllers\HomeController', 'myhomes']]
 ];
--- app/src/controllers/UserController.php
+++ app/src/controllers/UserController.php
@@ -26,6 +26,28 @@ class UserController extends Controller
         return $this->renderer->renderTemplate('user/register.php');
     }
 
+    public function registerpostAction($params){
+        if (!empty($_POST['firstname']) && !empty($_POST['surname']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmation']) && !empty($_POST['phone'])){
+            if ($_POST['password'] == $_POST['confirmation']){
+
+                $firstname = $_POST['firstname'];
+                $surname = $_POST['surname'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $phone = $_POST['phone'];
+
+                $this->users->insertUser($firstname, $surname, $email, $password, $phone);
+                return '<p>Votre inscription à été accepté, vous pouvez dorénavant vous connecter en cliquant ci-dessous :</p><p><a href="/login">Se connecter</a></p>';
+            }
+            else{
+                return '<p>Veuillez confirmer correctement votre mot de passe</p>';
+            }
+        }
+        else{
+            return '<p>Vous devez remplir tous les champs du formulaire</p>';
+        }
+    }
+
     public function loginpostAction($params) {
         $username = $_POST['email'];
         $password = $_POST['password'];
