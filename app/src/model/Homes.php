e03dcb2ec1c3752bf119662bd4ae188feeef0c8d
malobriant <malobriant@gmail.com>
2017-12-05 14:27:11 +0100

    model home

--- app/src/model/Homes.php
+++ app/src/model/Homes.php
@@ -4,7 +4,8 @@ namespace Src\Model;
 
 use \PDO;
 
-class Homes {
+class Homes
+{
 
     private $bdd;
 
@@ -13,12 +14,13 @@ class Homes {
      */
     public function __construct()
     {
-        $this->bdd = new PDO('mysql:host='.DB_HOST.';dbname=app', DB_USER, DB_PASSWORD);
+        $this->bdd = new PDO('mysql:host=' . DB_HOST . ';dbname=app', DB_USER, DB_PASSWORD);
         $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
 
-    public function getHomesByUserEmail($email) {
+    public function getHomesByUserEmail($email)
+    {
         $req = $this->bdd->prepare("
           SELECT apartment.*, (SELECT COUNT(*) FROM room WHERE apartmentId = apartment.id) AS roomsCount FROM apartment
           INNER JOIN user ON apartment.idUser = user.id
@@ -28,26 +30,59 @@ class Homes {
         return $req->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getRoomsByHomeId($homeId) {
+    public function getRoomsByHomeId($homeId)
+    {
         $req = $this->bdd->prepare("SELECT * FROM room WHERE apartmentId = :id");
         $req->execute([':id' => $homeId]);
         $res = $req->fetchAll(PDO::FETCH_ASSOC);
         return $res;
+    }
 
-    ###################
-    # COMMANDES HOMES #
-    ###################
+        ###################
+        # COMMANDES HOMES #
+        ###################
 
-    public function getAllHomes(){
+        public
+        function getAllHomes()
+        {
+            try {
+                $req = $this->bdd->query("SELECT * FROM user");
+                return $req->fetchAll(PDO::FETCH_ASSOC); // récupérer que la partie associative
+            } catch (\PDOException $e) {
+                return [];
+            } catch (\Error $e) {
+                return [];
+            }
+        }
+
+
+        public function getUserHomesOrdered()
+        {    //Donne les homes d'un user particulier repéré par son id, triée par code postal, et rue.
+            try {
+                $req = $this->bdd->prepare("SELECT * FROM apartment ORDER BY zipCode, street WHERE userId = :userId");  // Pas avec le nom et prénom car 2 clampins peuvent s'appeler pareil...
+                $req->execute("':userId' => $userId");
+                $res = $req->fetchALL(PDO::FETCH_ASSOC);
+                return $res;
+            } catch (\PDOException $e) {
+                return null;
+            } catch (\Exception $e) {
+                return null;
+            }
+        }
+
+
+        public function getManagerHomesOrdered(){    //Donne les homes d'un user particulier repéré par son id, triée par code postal, et rue.
         try {
-            $req = $this->bdd->query("SELECT * FROM user");
-            return $req->fetchAll(PDO::FETCH_ASSOC); // récupérer que la partie associative
+            $req = $this->bdd->prepare("SELECT * FROM apartment ORDER BY zipCode, street WHERE managerId = :managerId");  // Pas avec le nom et prénom car 2 clampins peuvent s'appeler pareil...
+            $req->execute("':managerId' => $managerId");
+            $res = $req->fetchALL(PDO::FETCH_ASSOC);
+            return $res;
         }
-        catch(\PDOException $e) {
-            return [];
+        catch (\PDOException $e) {
+            return null;
         }
-        catch(\Error $e) {
-            return [];
+        catch (\Exception $e){
+            return null;
         }
     }
 
