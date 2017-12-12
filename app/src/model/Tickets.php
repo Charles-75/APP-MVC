94c8bc4f0dad5ef90f053f1ee24cd02d02a8717e
malobriant <malobriant@gmail.com>
2017-12-12 11:20:14 +0100

    [APPMVC] Homes petite modif et Tickets (création de ticket et fermeture)

---
+++ app/src/model/Tickets.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Src\Model;
+
+
+class Ticket
+{
+    private $bdd;
+
+    /**
+     * Ticket constructor.
+     */
+    public function __construct()
+    {
+        $this->bdd = new PDO('mysql:host=' . DB_HOST . ';dbname=app', DB_USER, DB_PASSWORD);
+        $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    #####################
+    # COMMANDES Tickets #
+    #####################
+
+
+
+    public function createNewTicket($creationDate, $content) {    //Prendre la date actuelle dans le controller quand on appelle cette fonction
+                                                                  //Et la mettre dans creationDate
+
+        $req = $this->bdd->prepare("INSERT INTO ticket VALUE(:creationDate, :closeDate, :state, :content)");
+        $req->execute([
+            ':creationDate' => $creationDate,
+            ':closeDate' => null,
+            ':state' => 0,
+            ':content' => $content
+        ]);
+    }
+
+    public function closeTicket($tickedId, $closeDate){   //C'est juste un UPDATE avec le state qui change
+
+        //Mettre la date actuelle dans $closeDate dans le controller
+
+        $req = $this->bdd->prepare("UPDATE ticket SET closeDate = :closeDate state = :state WHERE id = :tickedId");
+        $req->execute([
+            ':closeDate' => $closeDate,
+            ':state' => 1,
+            ':tickedId' => $tickedId
+        ]);
+    }
+
+}
