b6f162bdaf05896b8130cd89499913c4d35b7b01
Rhett-Duncan <rhett-duncan.blond@laposte.net>
2017-12-13 21:25:31 +0100

    ajout de la page capteurs

---
+++ app/src/views/home/sensors.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Home ISEP</title>
+
+    <link rel="stylesheet" href="ressources/css/main.css">
+
+</head>
+
+<body>
+
+	<div class="block">
+		<div class="icone"> <!image du capteur>
+			<img src='' alt='image du capteur'>
+		</div>
+
+		<div class="text"><!description du capteur ?>
+			description du capteur = type + pièce
+		</div>
+
+		<div class="historic"><!historique des valeurs du capteur ?>
+			<sectionTitle>Historique</sectionTitle>
+		</div>
+
+		<div class="datas"><!graphique des valeurs du capteur ?>
+			<sectionTitle>Statistiques</sectionTitle>
+			<div class="diagram">
+				<div class='icone'>
+					<img src='' alt='diagramme 1'>
+				</div>
+				<div class='icone'>
+					<img src='' alt='diagramme 2'>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
