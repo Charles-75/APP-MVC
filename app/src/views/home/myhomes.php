09da65851253a94384a34c8c7fff8f0ff4f57997
Thibault HENRY <thibhenry92@gmail.com>
2017-12-22 11:29:49 +0100

    Design sur page de login et my homes

--- app/src/views/home/myhomes.php
+++ app/src/views/home/myhomes.php
@@ -8,30 +8,27 @@
 
     <div class="hover"><a href="/addhome" style="font-size: 150%"><img src="img/addHome.png" width="35px" height="35px" style="margin-top: 10px">  Ajouter une maison</a></div>
 
-    <?php foreach ($data as $value): ?>
-        <div class="card card-half">
-            <ul>
-                <div style="margin-top: 2%">
-                    <li>Ville : <?php echo $value['town']; ?></li>
-                    <li>Rue : <?php echo $value['street']; ?></li>
-                    <li>Numero : <?php echo $value['number']; ?></li>
-                    <li>Code Postal : <?php echo $value['zipCode']; ?></li>
-                </div>
-            </ul>
-            <div style="display: flex; margin-top: 2%">
-                <div style="margin-left: 2%"><button onclick="roomsRedirection(<?php echo $value['id'];?>)" class="bouton">Choisir</button></div>
-                <div style="margin-left: 2%"><button onclick="delete_confirm(<?php echo $value['id']?>)" class="boutonDelete">Supprimer</button></div>
-                <div style="margin-left: 2%"><?php
-                if ($value['id'] == $_SESSION['apartmentId']){
-                    ?>
-                    <img src="/img/checked.png" width="40px" height="40px">
-                    <?php
-                }
-                    ?>
+
+    <div class="homes-container">
+        <?php foreach ($data as $value): ?>
+            <div class="card home">
+                <iframe
+                        frameborder="0" class="map"
+                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyArELkdidNIXX2jnYgDvGSbqIgCQpdePMU&q=<?php echo urlencode($value['number'] . ' ' . $value['street'] . ' ' . $value['zipCode']) ?>" allowfullscreen>
+                </iframe class="map">
+                <div class="home-description">
+                    Ville : <?php echo $value['town']; ?><br>
+                    Rue : <?php echo $value['street']; ?><br>
+                    Numero : <?php echo $value['number']; ?><br>
+                    Code Postal : <?php echo $value['zipCode']; ?><br>
+                    <div class="home-buttons">
+                        <button onclick="roomsRedirection(<?php echo $value['id'];?>)" class="bouton">Choisir</button>
+                        <button onclick="delete_confirm(<?php echo $value['id']?>)" class="bouton bouton-delete">Supprimer</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 
 </div>
 
--- app/src/views/user/login.php
+++ app/src/views/user/login.php
@@ -1,22 +1,20 @@
-<div class="alignBox container">
+<div class="container align-box" id="login-container">
 
-    <div id="logo">
-        <img src="/img/Domisep.png" height="50%" width="50%">
-    </div>
+    <img src="/img/Domisep.png" class="img-responsive" id="login-logo">
 
-    <div class="card unique-card">
+    <div class="card">
 
         <form action="/loginpost" method="POST">
 
-            <input id="email" type="email" name="email" placeholder="Adresse e-mail" value="<?php if (isset($_COOKIE['email'])){ echo $_COOKIE['email'];} ?>">
+        <input id="email" type="email" name="email" placeholder="Adresse e-mail" value="<?php if (isset($_COOKIE['email'])){ echo $_COOKIE['email'];} ?>">
 
-            <input id="password" type="password" name="password" placeholder="Mot de passe" value="<?php if (isset($_COOKIE['password'])){ echo $_COOKIE['password'];} ?>">
+        <input id="password" type="password" name="password" placeholder="Mot de passe" value="<?php if (isset($_COOKIE['password'])){ echo $_COOKIE['password'];} ?>">
 
-            <div class="checkbox">
-                <input type="checkbox" name="rememberMe" id="rememberMe" <?php if (isset($_COOKIE['checked'])){ echo 'checked';} ?>>
-                <label for="rememberMe">Se souvenir de moi?</label>
-            </div>
-            <input type="submit" value="Connexion" id="submit" class="bouton">
+        <div class="checkbox">
+            <input type="checkbox" name="rememberMe" id="rememberMe" <?php if (isset($_COOKIE['checked'])){ echo 'checked';} ?>>
+            <label for="rememberMe">Se souvenir de moi?</label>
+        </div>
+        <input type="submit" value="Connexion" id="submit" class="bouton">
 
             <div class="liens">
                 <a href="/resetpassword">Mot de passe oublié?</a>
@@ -26,4 +24,5 @@
         </form>
 
     </div>
+
 </div>
