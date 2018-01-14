4f85aedb30e51fbc44c8ff3fa20472ec78d88894
paul bresnu <paul.bresnu@hotmail.fr>
2018-01-14 21:06:49 +0100

    ajout de la page room.php

---
+++ app/src/views/home/sensor.php
@@ -0,0 +1,33 @@
+<?php include(__DIR__."/../templates/main/navbar.php") ?>
+
+
+<div class="container">
+	<div class="block card">
+		<div class="icone"> <!image du capteur>
+			<img src='' alt='image du capteur'>
+		</div>
+
+		<div class="text"><!description du capteur ?>
+			description du capteur = type + pièce
+		</div>
+
+		<div class="historic"><!historique des valeurs du capteur ?>
+			<sectionTitle>Historique</sectionTitle>
+		</div>
+
+		<div class="datas"><!graphique des valeurs du capteur ?>
+			<sectionTitle>Statistiques</sectionTitle>
+			<div class="diagram">
+				<div class='icone'>
+					<img src='' alt='diagramme 1'>
+				</div>
+				<div class='icone'>
+					<img src='' alt='diagramme 2'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
