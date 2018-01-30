8946acbd557c73e9b2f89d6ac3cce2ce0c2c2282
Charles-75 <charles.boissy@hotmail.fr>
2018-01-30 20:21:02 +0100

    footbar pas affiché page login et register

--- app/src/views/templates/main/last.php
+++ app/src/views/templates/main/last.php
@@ -1,25 +1,19 @@
-    <footer>
-        <div class= "infoSite">
-            <?php
+<?php
 
-            if (!empty($_SESSION['id'])) {
-                echo '<div><a href="/cgu" class="footerLinks">CGU</a></div>
-                      <div><a href="/faq" class="footerLinks">FAQ</a></div>
-                      <div><a href="/contact"  class="footerLinks">Contact</a></div>
-                      <div><a href="/about"  class="footerLinks">À propos de Domisep</a></div>
-                      <div><a href="#"  class="footerLinks">S\'inscrire à la Newsletter</a></div>';
-            }
-            else{
-                echo '<div><a href="/login" class="footerLinks">CGU</a></div>
-                      <div><a href="/login" class="footerLinks">FAQ</a></div>
-                      <div><a href="/login"  class="footerLinks">Contact</a></div>
-                      <div><a href="/login"  class="footerLinks">À propos de Domisep</a></div>
-                      <div><a href="/login"  class="footerLinks">S\'inscrire à la Newsletter</a></div>';
-            }
-            ?>
+if (!empty($_SESSION['id'])) {
+    echo '<footer>
+            <div class= "infoSite">
+                <div><a href="/cgu" class="footerLinks">CGU</a></div>
+                <div><a href="/faq" class="footerLinks">FAQ</a></div>
+                <div><a href="/contact"  class="footerLinks">Contact</a></div>
+                <div><a href="/about"  class="footerLinks">À propos de Domisep</a></div>
+                <div><a href="#"  class="footerLinks">S\'inscrire à la Newsletter</a></div>
+            </div>
+          </footer>';
+    }
+?>
 
-        </div>
-    </footer>
+
 </div>
 </body>
 </html>
