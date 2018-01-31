f74fab021271eca7af1c41060c37ef9454bbb649
Rhett-Duncan <rhett-duncan.blond@laposte.net>
2018-01-31 03:45:32 +0100

    ajout newsletter et modif logo

---
+++ app/src/views/general/newsletter.php
@@ -0,0 +1,19 @@
+    <div class="container">
+
+      <div class="card">
+
+        <h1>S'inscrire à la newsletter</h1>
+
+        <form method="post" name="newsletter" action="newsletter-handler.php">
+
+            <input id="newsletterCheck" type="checkbox" name="newsletter">
+
+            <label for="newsletterCheck">Je souhaite m'inscrire à la newsletter</label>
+
+            <input type="submit" value="Valider" class="bouton">
+
+        </form>
+
+      </div>
+
+    </div>
