b26d2b756c6c30bd20972579a434a0760e1e244c
Orgrime <32840566+Orgrime@users.noreply.github.com>
2018-01-31 21:29:04 +0100

    test

--- app/src/views/home/test.php
+++ app/src/views/home/test.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Paul Bresnu
- * Date: 19/01/2018
- * Time: 09:51
- */
+
+
+<div>
+    <form method="POST" action="/">
+        <select id="selectStuff" name="stuff">
+            <?php  foreach ($data['room'] as $room) {     ?>
+
+                <option <?php  echo $room['id']; ?>><?php echo $room['name'] ?></option>
+        <?php } ?>
+        </select>
+        <select name="type" id="typeSelector"  ></select>
+
+
+        <input type="submit" value="simuler de nouvelles données">
+
+    </form>
+
+
+
+
+</div>
+<script>
+
+    var selector = document.getElementById('selectStuff');
+    var typeSelector = document.getElementById('typeSelector');
+    selector.addEventListener('change', openchoice);
+
+    function openchoice() {
+        var i;
+        var sensor=[];
+        while (typeSelector.firstChild) {
+            typeSelector.removeChild(typeSelector.firstChild);
+        }
+
+        var sensorTypes = JSON.parse("<?php foreach ($data['sensor'] as $sensor) {
+            echo addslashes(json_encode($sensor));
+        } ?>");
+        var choice = selector.value;
+
+        for (i = 0; i < sensorTypes.length; i++) {
+                if (sensorTypes['id']==choice){
+                    sensor.append(sensorTypes['id'],[sensorTypes['sensorId'],sensorTypes['reference']]);
+
+                }
+        }
+        var sensors =sensor[choice];
+        sensors.forEach(function(type){
+            typeSelector.appendChild(new Option(type['reference'], type['sensorId']));
+    });
+    }
+</script>
